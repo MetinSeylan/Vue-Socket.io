13bb9944e67b2367c2aeabf3a09b944e3e5eb96d
nomnes <nomnes1990@gmail.com>
2019-08-06 20:45:26 +0700

    update

--- index.d.ts
+++ index.d.ts
@@ -7,6 +7,7 @@ import {
     PropsDefinition,
 } from "vue/types/options";
 import { Vue } from "vue/types/vue";
+import { PluginFunction, PluginObject } from "vue";
 
 interface socketHandler<T> {
     (this: T, ...args: any[]): SocketIOClient.Socket
@@ -28,4 +29,22 @@ declare module 'vue/types/options' {
         Props=DefaultProps> {
         sockets: {[key: string]: socketHandler<V>}
     }
+}
+
+export interface VueSocketOptions {
+    debug?: boolean;
+    connection: string,
+    vuex?: {
+        store?: any,
+        actionPrefix?: string,
+        mutationPrefix?: string,
+        options?: {
+            useConnectionNamespace?: boolean
+        }
+    }
+}
+
+export default class VueSocketIO<T> implements PluginObject<T> {
+    constructor (options: VueSocketOptions);
+    install: PluginFunction<T>
 }
